1521f9bbfb15e19ce096861d881d707b7290eefc
laithalfaleh <laithalfaleh93@yahoo.com>
2025-07-21 11:11:19 +0300

    Create about.tsx

---
+++ pages/about.tsx
@@ -0,0 +1,10 @@
+// pages/about.tsx
+const AboutPage = () => {
+  return (
+    <div className="container mx-auto my-12 px-6">
+      <h1 className="text-4xl font-bold text-gray-800">من نحن</h1>
+      <p className="mt-4 text-lg text-gray-600">هنا سيتم وضع السيرة الذاتية للمحامي ليث الفالح والمحامي قصي الدروبي، ورؤية المجموعة.</p>
+    </div>
+  );
+};
+export default AboutPage;
