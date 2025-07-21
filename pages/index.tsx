a520f8ecdd0ab5c905e6c0828a52f1861f531bf5
laithalfaleh <laithalfaleh93@yahoo.com>
2025-07-21 11:06:54 +0300

    Create index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+// pages/index.tsx
+import type { NextPage } from 'next';
+
+const HomePage: NextPage = () => {
+  return (
+    <div>
+      <h1>الصفحة الرئيسية لمجموعة الصفوة</h1>
+    </div>
+  );
+};
+
+export default HomePage;
