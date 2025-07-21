d775c42c805c78ff396eac7e6461a389dfb0db23
laithalfaleh <laithalfaleh93@yahoo.com>
2025-07-21 11:11:46 +0300

    Create services.tsx

---
+++ pages/services.tsx
@@ -0,0 +1,10 @@
+// pages/services.tsx
+const ServicesPage = () => {
+  return (
+    <div className="container mx-auto my-12 px-6">
+      <h1 className="text-4xl font-bold text-gray-800">خدماتنا القانونية</h1>
+      <p className="mt-4 text-lg text-gray-600">هنا سيتم عرض قائمة مفصلة بالخدمات في القانون الخاص والعام والتحكيم.</p>
+    </div>
+  );
+};
+export default ServicesPage;
